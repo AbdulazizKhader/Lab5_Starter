200d805adb156a33d52294e40aa875d9a93cfc0b
AbdulazizKhader <akhader@ucsd.edu>
2024-05-07 23:54:14 -0700

    added tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,4 @@
 // unit.test.js
-
 import {
   isPhoneNumber,
   isEmail,
@@ -9,3 +8,53 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('is a phone number brackets', () => {
+  // TODO
+  expect(isPhoneNumber("(144)556-1677")).toBe(true);
+});
+test('is a phone number dashed', () => {
+  // TODO
+  expect(isPhoneNumber("124-343-4546")).toBe(true);
+});
+test('is not a phone number', () => {
+  // TODO
+  expect(isPhoneNumber("(-14)4556677")).toBe(false);
+});
+test('is not a phone number letter', () => {
+  // TODO
+  expect(isPhoneNumber("343434a32424")).toBe(false);
+});
+
+test('is an email normal', () => {
+  // TODO
+  expect(isEmail("ahshs@gmail.com")).toBe(true);
+});
+test('is an email numbers', () => {
+  // TODO
+  expect(isEmail("fewf22d@outlook.com")).toBe(true);
+});
+test('is not an email brackets', () => {
+  // TODO
+  expect(isEmail("(fewf22d)@outlook.com")).toBe(false);
+});
+test('is not an email symbols', () => {
+  // TODO
+  expect(isEmail("_+fewf22d@outlook.com")).toBe(false);
+});
+
+test('is a strong password normal', () => {
+  // TODO
+  expect(isStrongPassword("hhdhdHHdd2323")).toBe(true);
+});
+test('is a stong Password 4 letters', () => {
+  // TODO
+  expect(isStrongPassword("fdH4")).toBe(true);
+});
+test('is not a strong password short', () => {
+  // TODO
+  expect(isStrongPassword("225")).toBe(false);
+});
+test('is nota stong Password 16 letters', () => {
+  // TODO
+  expect(isStrongPassword("hfhgkdlskdjgkfne")).toBe(false);
+});
