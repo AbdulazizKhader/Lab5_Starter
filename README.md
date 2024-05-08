04d52427b457445a90ed8c77298aea5ac6dfff09
AbdulazizKhader <akhader@ucsd.edu>
2024-05-07 22:00:15 -0700

    Finished expose.js except the confetti part

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 5 - Starter
+Member Names:
+Abdulaziz Khader
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,46 @@
 // expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  //don't want it to change so use const
+  const hornMain = document.getElementById("horn-select"); //input is id
+  const hornPic = document.querySelector("img"); //gets the first image
+  const volumePic = document.querySelector("div#volume-controls img"); //gets the image inside the div with volume-controls id
+  const volumeSoundIcon = document.getElementById("volume");
+  const audio = document.querySelector("audio");
+  const audioButton = document.querySelector("button"); //need to configure the button
+
+  hornMain.addEventListener("change", () => {
+    let hornPicChoice = hornMain.value;
+    hornPic.src = "assets/images/" + hornPicChoice + ".svg";
+    hornPic.alt = hornPicChoice;
+    audio.src = "assets/audio/" + hornPicChoice + ".mp3";
+  })
+
+  volumeSoundIcon.addEventListener("change", () => {
+    let sound = volumeSoundIcon.value;
+    if(sound == 0){
+      volumePic.src = "assets/icons/volume-level-0.svg";
+      volumePic.alt = "Volume level 0"
+    }
+    if(sound > 0 && sound < 33){
+      volumePic.src = "assets/icons/volume-level-1.svg";
+      volumePic.alt = "Volume level 1";
+    }
+    if(sound >= 33 && sound < 67){
+      volumePic.src = "assets/icons/volume-level-2.svg";
+      volumePic.alt = "Volume level 2";
+    }
+    if(sound >= 67){
+      volumePic.src = "assets/icons/volume-level-3.svg";
+      volumePic.alt = "Volume level 3";
+    }
+    audio.volume = sound / 100;
+  })
+
+  audioButton.addEventListener("click", () => {
+    audio.play();
+  })
+  
 }
